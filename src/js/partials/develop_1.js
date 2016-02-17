d0abbb5c1dbd55ce5a9870a14b6c9a4c42ea00cb
Dmitry <dmitry1939@gmail.com>
2016-02-17 12:19:54 +0200

    paralax element prety show by window.load

--- src/js/partials/develop_1.js
+++ src/js/partials/develop_1.js
@@ -1,4 +1,58 @@
+
+function topBlockParalax(){
+
+    // paralax move func
+
+    function moveParalax(scroll){
+        $('.paralax-layer').each(function() {
+
+            var count = $(this).data('count');
+            var max = $(this).data('max');
+            var way = $(this).data('way');
+
+            var value = count * scroll;
+
+            if(value >= max){
+                value = max;
+            }
+            if(way == "top"){
+                value=value*(-1);
+            }
+
+            $(this).css({'transform':'translateY('+value+'px)'});
+
+        });
+    }
+
+    // paralax load position
+
+    var scroll = $(window).scrollTop();
+    moveParalax(scroll);
+
+    // paralax position by scroll move
+
+    $(window).scroll(function(){
+
+        scroll = $(window).scrollTop();
+        moveParalax(scroll);
+
+    });
+
+    // prety show paralax elements by page load
+
+    $(window).load(function(){
+
+        setTimeout(function(){
+            $('.paralax-background').addClass('show');
+        }, 500);
+
+    });
+
+}
+
 $(document).ready(function(){
+
+    topBlockParalax();
 
 });
 
